80da0002cda2d3d3a1c5a7531999006e6677dc12
sagesta <adebodun93@gmail.com>
2026-01-01 21:00:06 +0100

    Fix: Align index.js variables with function.json binding names

--- api/GetVisitorCount/index.js
+++ api/GetVisitorCount/index.js
@@ -1,26 +1,31 @@
-module.exports = async function (context, req, inputDocument) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+module.exports = async (context, _req, inputDocument) => {
+	context.log("JavaScript HTTP trigger function processed a request.");
+	context.log(inputDocument);
 
-    let currentCount = 0;
+	let currentCount = 0;
 
-    if (inputDocument) {
-        currentCount = inputDocument.count;
-    }
+	if (inputDocument) {
+		currentCount = inputDocument.count || 0;
+	} else {
+		context.log.warn(
+			"Cosmos DB input document not found. A new document will be created.",
+		);
+	}
 
-    const newCount = currentCount + 1;
+	const newCount = currentCount + 1;
 
-    // Response body
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: {
-            count: newCount
-        }
-    };
+	// Response body
+	context.res = {
+		// status: 200, /* Defaults to 200 */
+		body: {
+			count: newCount,
+		},
+	};
 
-    // Output to Cosmos DB to update the count
-    context.bindings.outputDocument = {
-        id: "1",
-        count: newCount,
-        partitionKey: "1"
-    };
-}
+	// Output to Cosmos DB to update the count
+	context.bindings.outputDocument = {
+		id: "1",
+		count: newCount,
+		partitionKey: "1",
+	};
+};
